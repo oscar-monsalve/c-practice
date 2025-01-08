34e98b3f6610c37e03282428a17a129fe5e35a11
oscar monsalve <oscarmonsalvec5@gmail.com>
2025-01-08 12:51:54 -0500

    feat: update

--- exercism/00_leap.c
+++ exercism/00_leap.c
@@ -10,13 +10,27 @@
 //     1900 was not a leap year as it's not divisible by 400.
 //     2000 was a leap year!
 
-#include <stdio.h>
-
-int askYear() {
-    char input;
+_Bool leap_year(int year) {
+    if (year % 100 == 0) {
+        if (year % 400 == 0) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+    else if (year % 4 == 0) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
 }
 
 int main() {
+
+    leap_year(2015);
+    leap_year(1970);
 
     return 0;
 }
